ef47295baf9baf84bcd604aca9f85e11717c3bf9
JanelleG51 <janelle_macmillan@yahoo.co.uk>
2021-05-09 22:53:53 +0000

    func: add function outlines to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,25 @@
 
+function startGame() {
+   	$( "div" ).removeClass( "mole" ) 
+}
+
+
+function randomPosition(){
+
+}
+
+function captureTimes(){
+
+}
+
+function returnResults(){
+
+} 
+
+function endGame(){
+    
+}
+
+
+
+
