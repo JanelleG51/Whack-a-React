d4438640dfd18a082660e9cf5c9bab5fad0e085f
JanelleG51 <janelle_macmillan@yahoo.co.uk>
2021-05-11 18:30:18 +0000

    change: remove grid system from game.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,11 @@
 
-function startGame() {
-   	$( "div" ).removeClass( "mole" ) 
-}
 
 
-function randomPosition(){
+//function captureTimes(){}
 
-}
+//function returnResults(){} 
 
-function captureTimes(){
-
-}
-
-function returnResults(){
-
-} 
-
-function endGame(){
-    
-}
+//function endGame(){}
 
 
 
