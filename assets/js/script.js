3cbb0fd677e05664466174a3e86b937f1bb2e989
JanelleG51 <janelle_macmillan@yahoo.co.uk>
2021-05-13 21:07:56 +0000

    script: add capture each reaction time in an array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,49 @@
-let start = new Date().getTime();
+function ready() {
 
-function makeCuteMoleAppear() {
+    const start = new Date().getTime();
+    const timer = document.getElementById("time-left");
+}
+
+let currentTime = 20;
+let totalTime = [];
+    
+
+function makeCuteMoleAppear() { //makes mole appear at random position on screen and starts timer
     let top = Math.random() * 200;
     let left = Math.random() * 300;
     let width = (Math.random() * 200) + 100;
-    
 
     document.getElementById("mole").style.width = width + "px";
     document.getElementById("mole").style.height = width + "px";
     document.getElementById("mole").style.top = top + "px";
     document.getElementById("mole").style.left = left + "px";
     document.getElementById("mole").style.display = "block";
-    
 
     start = new Date().getTime();
 }
 
-function appearAfterDelay() {
+function appearAfterDelay() { //sets the delay between appearances 
     setTimeout(makeCuteMoleAppear, Math.random() * 3000);
 }
 
 appearAfterDelay();
 
-document.getElementById("mole").onclick = function() {
+document.getElementById("mole").onclick = function () { //captures reaction time and removes mole from screen
     document.getElementById("mole").style.display = "none";
 
-    let end = new Date().getTime();
-    let timeTaken = (end - start) / 1000;
+    let end = new Date().getTime();  
+    let timeTaken = (end - start) / 1000; //returns users reaction time
     document.getElementById("reaction-time").innerHTML = timeTaken + "s";
+    let totalAverage = timeTaken;
+    totalTime.push(totalAverage);
+    appearAfterDelay(); //resets game play 
+};
 
-    appearAfterDelay();
-}
+console.log(totalTime);
 
+//function timer 
 
-//function captureTimes(){}
 
 //function returnResults(){} 
 
 //function endGame(){}
-
-
-
-
